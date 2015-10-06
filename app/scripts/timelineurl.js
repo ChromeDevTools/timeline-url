c8dba739c3081cabf93b5f7e1bb896c7c44ab7ad
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-10-06 18:10:05 +0200

    Fix bug because of blink/chromium merge
    As a result I ended up cleaning up a bit of the code.
    
    Fix #21

--- app/scripts/timelineurl.js
+++ app/scripts/timelineurl.js
@@ -14,47 +14,34 @@ class TimelineUrl {
 		this.outputUrl = undefined;
 
 		this.isUrlCORS = false;
-		this.revInfo = {};
+		this.revs = {};
 
 		this.generateUrl();
 	}
 
 	getRevs() {
 		var chromeVersion = navigator.appVersion.replace(/.+Chrome\/(.+) .+/, '$1');
+		var url = 'https://omahaproxy.appspot.com/revision.json?version=' + chromeVersion;
 
-		var revInfo = {};
-		var revUrls = ['https://omahaproxy.appspot.com/revision.json?version=',
-			'https://omahaproxy.appspot.com/webkit.json?version='];
-
-		// take our two URLs, fetch them in parallel, parse to JSON, and merge.
-		return revUrls.map(function (url) {
-			return fetch(url + chromeVersion);
-		}).reduce(function handleJSON(chain, revPromise) {
-			return revPromise.then(function (payload) {
-				return payload.json();
-			}).then(function (json) {
-				return Object.assign(revInfo, json);
-			});
-		}, Promise.resolve()).then(function(revInfo){
-			// {"chromium_base_position":"338390","blink_position":"198714", …
-			this.revInfo = revInfo;
+		return fetch(url).then(function (payload) {
+			return payload.json();
+		}).then(function (revs) {
+			this.revs = revs;
 		}.bind(this));
-
 	}
 
 	getUrl(url) {
-
-		var chromeRev = this.revInfo.chromium_base_position;
-		var blinkRev = this.revInfo.blink_position;
+		var position = this.revs.chromium_base_position;
+		var commit = this.revs.chromium_base_commit;
 
 		return [
 			this.isUrlCORS ? 'https://frontend.chrome-dev.tools/serve_rev/@' :
 				'chrome-devtools://devtools/remote/serve_rev/@',
 
-			blinkRev, // e.g. 198714
+			commit, // e.g. 198714
 
 			// Devtools previously used devtools.html : codereview.chromium.org/1144393004/
-			chromeRev < 332419 ? '/devtools.html' : '/inspector.html',
+			position < 332419 ? '/devtools.html' : '/inspector.html',
 
 			'?loadTimelineFromURL=', url
 		].join('');
